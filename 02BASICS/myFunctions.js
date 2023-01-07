c2b5b984cfceaf0ecd77ab9f7d5d212124f78f0f
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-07 16:45:52 +0100

    function commit 1

---
+++ 02BASICS/myFunctions.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    return num + 2;
+}
+;
+var getUpper = function (val) {
+    return val.toUpperCase();
+};
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+addTwo(5);
+getUpper('Oluwaseun');
+signUpUser('Hi', "hit@gmail.com", true); 
+loginUser('Janet', "jane@ebay.com");
---
+++ 02BASICS/myFunctions.ts
@@ -0,0 +1,20 @@
+function addTwo(num: number): number {
+    return num + 2;
+};
+
+let getUpper = (val: string) => {
+    return val.toUpperCase();
+};
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+addTwo(5);
+getUpper('Oluwaseun');
+
+signUpUser('Hi', "hit@gmail.com", true);
+loginUser('Janet', "jane@ebay.com")
+
+
+export {}
