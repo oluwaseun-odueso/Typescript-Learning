0f4bd3a94f370de9769deb3f18c5d32e82b77dd9
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-07 17:24:44 +0100

    second function commit

--- 02BASICS/myFunctions.js
+++ 02BASICS/myFunctions.js
@@ -13,5 +13,19 @@ var loginUser = function (name, email, isPaid) {
 };
 addTwo(5);
 getUpper('Oluwaseun');
-signUpUser('Hi', "hit@gmail.com", true); 
+signUpUser('Hi', "hit@gmail.com", true);
 loginUser('Janet', "jane@ebay.com");
+var getHello = function (s) {
+    return "You";
+};
+var heros = ["Thor", "Spiderman", "Ironman"];
+// const heros = [1, 2, 3]
+heros.map(function (hero) {
+    return "Hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- 02BASICS/myFunctions.ts
+++ 02BASICS/myFunctions.ts
@@ -17,4 +17,23 @@ signUpUser('Hi', "hit@gmail.com", true);
 loginUser('Janet', "jane@ebay.com")
 
 
+const getHello = (s: string) => {
+    return "You"
+}
+
+const heros = ["Thor", "Spiderman", "Ironman"]
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return `Hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg)
+}
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg)
+}
+
 export {}
