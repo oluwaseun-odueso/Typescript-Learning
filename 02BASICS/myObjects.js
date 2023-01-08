1611bd6e8ae7b52bffeadc9ecc77afdce2a01eef
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-08 21:56:41 +0100

    Read only, optional field, mix and match of types

--- 02BASICS/myObjects.js
+++ 02BASICS/myObjects.js
@@ -5,7 +5,14 @@
 //     isActive: true
 // };
 exports.__esModule = true;
-function createUser(user) {
-    return { name: "", email: "", isActive: true };
-}
-createUser({ name: "", email: "", isActive: true });
+var myUser = {
+    _id: "1234",
+    name: "boy",
+    email: "boy@mail.com",
+    isActive: true
+};
+// Mix and match of types
+myUser.email = "boyles@gmail.com";
+// Cannot reassign read only values
+// myUser._id = "234"
+console.log(myUser);
--- 02BASICS/myObjects.ts
+++ 02BASICS/myObjects.ts
@@ -13,18 +13,57 @@
 // function createCourse(): {name: string, price: number} {
 //     return {name: "Adebola", price: 5000};
 // };
-type myString = string;
+// type myString = string;
 
+// type User = {
+//     name: myString; 
+//     email: string;
+//     isActive: boolean
+// }
+
+// function createUser(user: User): User {
+//     return {name: "", email: "", isActive: true}
+// }
+
+// createUser({name: "", email: "", isActive: true})
+
+// Use read only keyword for values that you do not want to be altered
+// Add question mark behind values you want to declare optional
 type User = {
-    name: myString; 
+    readonly _id: string
+    name: string;
     email: string;
-    isActive: boolean
+    isActive: boolean;
+    creditCard?: number;
 }
 
-function createUser(user: User): User {
-    return {name: "", email: "", isActive: true}
+let myUser: User = {
+    _id: "1234",
+    name: "boy",
+    email: "boy@mail.com",
+    isActive: true
+}
+ 
+// Mix and match of types: Combining types
+type cardNumber = {
+    cardnumber: string
 }
 
-createUser({name: "", email: "", isActive: true})
+type cardDate = {
+    cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+
+// Mix and match of types
+
+myUser.email = "boyles@gmail.com";
+// Cannot reassign read only values
+// myUser._id = "234"
+
+console.log(myUser)
+
 
 export {};
