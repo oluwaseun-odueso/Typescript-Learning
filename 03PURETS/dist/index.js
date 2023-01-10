cf7c41c3eca59d075aaa65d4b11b181901b31940
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-10 14:32:35 +0100

    classes 2

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,13 +1,23 @@
 "use strict";
+// class User {
+//     // Every field you don't state as private is public
+//     email: string;
+//     name: string;
+//     // You can make a field inaccessible by using the private keyword, it can only be accessed within the class
+//     private readonly city: string = "Lagos"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     };
+// };
+// A class can also be written concisely as this
 class User {
-    constructor(email, name) {
-        this.city = "Just";
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this.city = "Lagos";
     }
-    ;
 }
-;
-const justice = new User("justice@g.com", "Justice");
-justice.city = "Justi";
-console.log(justice.city);
+const justice = new User("justice@g.com", "Justice", 1);
+// justice.city = "Justi"
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,13 +1,24 @@
-class User {
-    email: string;
-    name: string;
-    readonly city: string = "Just"
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
-    };
-};
+// class User {
+//     // Every field you don't state as private is public
+//     email: string;
+//     name: string;
+//     // You can make a field inaccessible by using the private keyword, it can only be accessed within the class
+//     private readonly city: string = "Lagos"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     };
+// };
 
-const justice = new User("justice@g.com", "Justice");
+// A class can also be written concisely as this
+class User {
+    readonly city: string = "Lagos";
+    constructor(
+        public email: string, 
+        public name: string, 
+        private userId: number) {}
+}
+
+
+const justice = new User("justice@g.com", "Justice", 1);
 // justice.city = "Justi"
-console.log(justice.city)
