f5d2e3f44e19e31e83b92de1f2315a6d658312cc
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-10 14:03:29 +0100

    classes 1

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('illinois');
+class User {
+    constructor(email, name) {
+        this.city = "Just";
+        this.email = email;
+        this.name = name;
+    }
+    ;
+}
+;
+const justice = new User("justice@g.com", "Justice");
+justice.city = "Justi";
+console.log(justice.city);
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,1 +1,13 @@
-console.log('illinois');
+class User {
+    email: string;
+    name: string;
+    readonly city: string = "Just"
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    };
+};
+
+const justice = new User("justice@g.com", "Justice");
+// justice.city = "Justi"
+console.log(justice.city)
