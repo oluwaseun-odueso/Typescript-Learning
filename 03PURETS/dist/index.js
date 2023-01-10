755cca071d4e369a413cf9f5aff33f38ed7e80cf
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-10 14:55:50 +0100

    getters and setters

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -16,8 +16,34 @@ class User {
         this.email = email;
         this.name = name;
         this.userId = userId;
+        this._courseCount = 1;
         this.city = "Lagos";
     }
+    ;
+    // There could be private methods as well
+    deleteToken() {
+        console.log("Token deleted.");
+    }
+    // Getters in Typescript
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    ;
+    get courseCount() {
+        return this._courseCount;
+    }
+    ;
+    // NB: A setter should have no return type
+    // NB: Also, setters cannot return a value
+    // NB: A setter does not have a return type because it usually have no return value
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        ;
+        this._courseCount = courseNum;
+    }
+    ;
 }
 const justice = new User("justice@g.com", "Justice", 1);
 // justice.city = "Justi"
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -12,11 +12,38 @@
 
 // A class can also be written concisely as this
 class User {
+
+    private _courseCount = 1;
     readonly city: string = "Lagos";
+
     constructor(
         public email: string, 
         public name: string, 
-        private userId: number) {}
+        private userId: number) {};
+
+    // There could be private methods as well
+    private deleteToken() {
+        console.log("Token deleted.")
+    }
+
+    // Getters in Typescript
+    get getAppleEmail(): string {
+        return `apple ${this.email}`;
+    };
+
+    get courseCount(): number {
+        return this._courseCount;
+    };
+
+    // NB: A setter should have no return type
+    // NB: Also, setters cannot return a value
+    // NB: A setter does not have a return type because it usually have no return value
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        };
+        this._courseCount = courseNum;
+    };
 }
 
 
