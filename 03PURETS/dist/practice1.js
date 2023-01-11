f84554d6e24197ede18b5e8e61e9b84c5f9a92cb
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-11 23:42:06 +0100

    Second commit for task

--- 03PURETS/dist/practice1.js
+++ 03PURETS/dist/practice1.js
@@ -1,17 +1,68 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+// const writeeData = (filename, obj) => {
+//     return new Promise((resolve, reject) => {
+//         const newObj = JSON.stringify(obj)
+//         writeFile(filename, newObj, (err, data) => {
+//             if (err) throw err;
+//         })
+//     })
+// }
 class Posts {
     constructor(details) {
         this.details = details;
     }
     ;
-    set post(details) {
-        this.details = details;
+    setPost(filename, details) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const newObj = JSON.stringify(details);
+                (0, fs_1.writeFile)(filename, newObj, (err) => {
+                    if (err)
+                        throw err;
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        });
     }
-    ;
-    get post() {
-        return this.details;
+    getPost(filename) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                (0, fs_1.readFile)(filename, 'utf8', (err, data) => {
+                    if (err)
+                        throw err;
+                    const postData = JSON.parse(data);
+                    return postData;
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        });
     }
-    ;
+    // get post(): PostInterface {
+    //     // return this.details;
+    //     return new Promise((resolve, reject) => {
+    //         readFile(filename, 'utf8', (err, data) => {
+    //             if (err) throw err;
+    //             const a = JSON.parse(data);
+    //             resolve(a);
+    //             reject(new Error)
+    //         })  
+    //     })
+    // };
     updatePost(details) {
         this.details = details;
         return details;
--- 03PURETS/src/practice1.ts
+++ 03PURETS/src/practice1.ts
@@ -1,3 +1,5 @@
+import {NoParamCallback, readFile, writeFile} from 'fs'
+
 // Post Interface
 interface PostInterface {
     readonly id: number,
@@ -22,18 +24,57 @@ const enum FoulLanguages {
     useless
 }
 
+// const writeeData = (filename, obj) => {
+//     return new Promise((resolve, reject) => {
+//         const newObj = JSON.stringify(obj)
+//         writeFile(filename, newObj, (err, data) => {
+//             if (err) throw err;
+//         })
+//     })
+// }
+
 class Posts {
     constructor (
         public details: PostInterface
     ) {};
 
-    set post(details) {
-        this.details = details;
-    };
+    async setPost (filename: string, details: object) {
+        try {
+            const newObj: string = JSON.stringify(details);
+            writeFile(filename, newObj, (err) => {
+                if (err) throw err
+            })
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getPost (filename: string) {
+        try {
+           readFile(filename, 'utf8', (err, data) => {
+            if (err) throw err
+
+            const postData = JSON.parse(data);
+            return postData;
+           }) 
+        } catch (error) {
+            throw error
+        }
+    }
     
-    get post(): PostInterface {
-        return this.details;
-    };
+    // get post(): PostInterface {
+    //     // return this.details;
+
+    //     return new Promise((resolve, reject) => {
+    //         readFile(filename, 'utf8', (err, data) => {
+    //             if (err) throw err;
+    
+    //             const a = JSON.parse(data);
+    //             resolve(a);
+    //             reject(new Error)
+    //         })  
+    //     })
+    // };
 
     updatePost(details: PostInterface): PostInterface {
         this.details = details;
