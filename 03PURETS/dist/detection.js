0db2d48566bd29b8278d22bbef2f00a4f434fbe6
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-11 13:52:53 +0100

    Type narrowing third commit

--- 03PURETS/dist/detection.js
+++ 03PURETS/dist/detection.js
@@ -39,3 +39,26 @@ function isAdminForAccount(account) {
     }
     ;
 }
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+    ;
+}
+;
+function isFish(pet) {
+    return pet.swim != undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
